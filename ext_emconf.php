f6f36ad991db1d0bfc0e0836cf5b2203617fa24e
tomitzel <tomitzel@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-08 12:58:56 +0000

    Initial commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tm_hr/trunk@44766 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,58 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "tm_hr".
+#
+# Auto generated 25-02-2011 08:41
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TM Human Resources',
+	'description' => 'Human Resources Management extension with support for:
+    * Workforce planning
+    * Recruitment (sometimes separated into attraction and selection)
+    * Induction, Orientation and Onboarding
+    * Skills management
+    * Training and development
+    * Personnel administration
+    * Compensation in wage or salary
+    * Time management
+    * Travel management (sometimes assigned to accounting rather than HRM)
+    * Payroll (sometimes assigned to accounting rather than HRM)
+    * Employee benefits administration
+    * Personnel cost planning
+    * Performance appraisal
+    * Labor relations',
+	'category' => 'plugin',
+	'author' => 'Tomita Militaru',
+	'author_email' => 'mail@tomitamilitaru.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"21a8";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"928b";s:20:"doc/wizard_form.html";s:4:"7553";}',
+);
+
+?>
